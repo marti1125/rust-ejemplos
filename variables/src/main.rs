3b79b1b9e324afee3a0bd58b89df5f0abb77135e
Willy Aguirre <marti1125@gmail.com>
2015-09-30 18:51:43 -0500

    se agrega un ejemplo sobre variables

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let (a, b) = (1, 2);
+
+    let x: i32 = 5;
+
+    let c = 5; // x: i32
+
+    println!("Pattern Variable, {:?}", (a, b));
+
+    println!("Pattern Variable (a), {:?}", a);
+
+    println!("Varible, {:?}", b);
+
+}
