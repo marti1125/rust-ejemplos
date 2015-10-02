5a570b8069f88538e77ffa214f090f08f43ef3be
Willy Aguirre <marti1125@gmail.com>
2015-10-02 16:04:38 -0500

    se agrega un ejemplo de funciones

---
+++ funciones/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    saludo("Mozilla".to_string());
+    println!("Suma: {:?}", sumar(100,898));
+    println!("Potencia: {:?}", potencia(5,5));
+    let total = sumar(100,898) + potencia(5,5);
+    println!("Total: {:?}", total);
+}
+
+fn saludo(name :String){
+    println!("Hello, {:?}", name)
+}
+
+fn sumar(x :i32, y :i32) -> i32 {
+    x + y
+}
+
+fn potencia(x :i32, y :i32) -> i32 {
+    let mut result = 1;
+    for i in 1..y+1 {
+        result = result * x;
+        //println!("Respuesta, {:?}", result);
+    }
+    result
+}
